27823ec1ae64d4153a9234c20268ff294f869d0b
tnantoka <tnantoka@bornneet.com>
2018-11-03 22:05:18 +0900

    Show sky, star

--- src/index.js
+++ src/index.js
@@ -1,45 +1,31 @@
-var config = {
+const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
-  },
   scene: {
     preload,
-    create
+    create,
+    update
   }
 };
 
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.setBaseURL('./');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', 'assets/star.png');
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude', 'assets/dude.png', {
+    frameWidth: 32,
+    frameHeight: 48
+  });
 }
 
 function create() {
   this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
+}
 
-  var particles = this.add.particles('red');
-
-  var emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD'
-  });
-
-  var logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
+function update() {
 }
