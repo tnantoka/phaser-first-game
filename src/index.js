864b5685a36f7b226acc6010b281f0eb06b254d8
tnantoka <tnantoka@bornneet.com>
2018-11-04 22:00:53 +0900

    Add stars

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-        gravity: { y: 300 },
-        debug: false
+      gravity: { y: 300 },
+      debug: false
     }
   },
 
@@ -50,41 +50,58 @@ function create() {
   player.setCollideWorldBounds(true);
 
   this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
   });
 
   this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'dude', frame: 4 } ],
-      frameRate: 20
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
   });
 
   this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  const stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
   });
 
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+
   cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
   if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-      player.anims.play('left', true);
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
   } else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-      player.anims.play('right', true);
+    player.setVelocityX(160);
+    player.anims.play('right', true);
   } else {
-      player.setVelocityX(0);
-      player.anims.play('turn');
+    player.setVelocityX(0);
+    player.anims.play('turn');
   }
   if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
+    player.setVelocityY(-330);
   }
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
 }
